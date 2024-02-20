938c44dcfe09768a58be69370f38dc915807d80d
LIM GEN JACK <135495371+Jack-1118@users.noreply.github.com>
2024-02-20 15:20:46 +0800

    add

--- js/index.js
+++ js/index.js
@@ -7,12 +7,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
         var countryCode = document.getElementById('country-code').value;
         var mobileNumber = document.getElementById('mobile-number').value;
         
-        // Validate the input
+        
         if (countryCode + mobileNumber === "+60173527250") {
-            // Redirect to the second page
-            window.location.href = 'page2.html'; // Replace with the actual URL of your second page
+            
+            window.location.href = 'page2.html?phone=' + countryCode + mobileNumber;
         } else {
-            // Alert the user if the input is not the valid number
+            
             alert('No such number found! Please try again.');
         }
     });
